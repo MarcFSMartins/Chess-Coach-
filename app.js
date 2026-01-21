5d62c75ca2de31484fe5722fa4969f9c225a14b3
MarcFSMartins <88166308+MarcFSMartins@users.noreply.github.com>
2026-01-21 18:36:23 -0300

    Refactor board initialization and class names

--- app.js
+++ app.js
@@ -19,16 +19,19 @@ const initialBoard = [
   "RNBQKBNR"
 ];
 
-let board = JSON.parse(JSON.stringify(initialBoard));
+let board = structuredClone(initialBoard);
 
 function render() {
   boardEl.innerHTML = "";
+
   for (let r = 0; r < 8; r++) {
     for (let c = 0; c < 8; c++) {
       const sq = document.createElement("div");
-      sq.className = "square " + ((r + c) % 2 ? "black" : "white");
+      sq.className = "square " + ((r + c) % 2 === 0 ? "light" : "dark");
+
       const piece = board[r][c];
       if (piece !== ".") sq.textContent = pieces[piece];
+
       sq.onclick = () => onSquareClick(r, c, sq);
       boardEl.appendChild(sq);
     }
@@ -50,7 +53,7 @@ function onSquareClick(r, c, el) {
       board[sr].substring(sc + 1);
 
     selected = null;
-    statusEl.innerText = "Lance executado";
+    statusEl.innerText = "Lance feito";
     render();
   } else if (board[r][c] !== ".") {
     selected = [r, c];
@@ -60,7 +63,7 @@ function onSquareClick(r, c, el) {
 }
 
 function resetGame() {
-  board = JSON.parse(JSON.stringify(initialBoard));
+  board = structuredClone(initialBoard);
   selected = null;
   statusEl.innerText = "Partida reiniciada";
   render();
