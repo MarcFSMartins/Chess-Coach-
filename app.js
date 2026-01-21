791ab868814bd59fc801a681693fea1795a43384
MarcFSMartins <88166308+MarcFSMartins@users.noreply.github.com>
2026-01-21 17:49:41 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,20 +1,55 @@
 const game = new Chess();
 
-const board = Chessboard('board', {
-  draggable: true,
-  position: 'start',
-  onDrop: onDrop
+const boardElement = document.getElementById("board");
+
+const ground = Chessground(boardElement, {
+  orientation: "white",
+  movable: {
+    free: false,
+    color: "white",
+    dests: getDests()
+  }
 });
 
-function onDrop(source, target) {
+function getDests() {
+  const dests = new Map();
+  game.SQUARES.forEach(s => {
+    const moves = game.moves({ square: s, verbose: true });
+    if (moves.length) {
+      dests.set(
+        s,
+        moves.map(m => m.to)
+      );
+    }
+  });
+  return dests;
+}
+
+ground.set({
+  movable: {
+    dests: getDests(),
+    events: {
+      after: onMove
+    }
+  }
+});
+
+function onMove(from, to) {
   const move = game.move({
-    from: source,
-    to: target,
-    promotion: 'q'
+    from,
+    to,
+    promotion: "q"
   });
 
-  if (move === null) return 'snapback';
+  if (!move) return;
 
   document.getElementById("status").innerText =
     "Você jogou: " + move.san;
+
+  ground.set({
+    fen: game.fen(),
+    movable: {
+      dests: getDests()
+    }
+  });
 }
